13112e08de575fd686610f7ccfce2cd3f60ae757
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 17:35:30 -0300

    style: estilo da logo

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,15 +2,11 @@ import styled from 'styled-components';
 
 const ContainerStyle = styled.header`
   display: flex;
-  /* background: red linear-gradient(#c1c1c1, #fff); */
   height: 100px;
   
-
   > img {
-    width: 180px;
-    /* height: 100px; */
-    /* margin: 0 auto; */
-    margin: auto 15px;
+    width: 90px;
+    margin: auto 85px;
   }
 `;
 
