de7832d0512019ef44861f274581bee52200821d
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 15:58:07 -0300

    style: implementa estilos para o componente SideBar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Container } from './style';
+import { SideBarContainer, SidebarHeading, SidebarLink } from './style';
 
 
 function SideBar() {
   return (
-    <Container>
-      <h1>SideBar</h1>
-
-    </Container>
+    <SideBarContainer>
+      <SidebarHeading>Menu de Links</SidebarHeading>
+      <SidebarLink to="/">1</SidebarLink>
+      <SidebarLink to="/">2</SidebarLink>
+      <SidebarLink to="/">3</SidebarLink>
+      <SidebarLink to="/">4</SidebarLink>
+    </SideBarContainer>
   )
 }
 
--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,10 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-`; 
+export const SideBarContainer = styled.div`
+  width: 250px;
+  height: 100vh;
+  background-color: #f0f0f0;
+  padding: 20px;
+`;
+
+export const SidebarLink = styled.a`
+  display: block;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  color: #333;
+  text-decoration: none;
+  &:hover {
+    color: #555;
+  }
+`;
+
+  export const SidebarHeading = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 20px;
+`;
