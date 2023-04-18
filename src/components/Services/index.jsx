d3561975e4587ddbfdfb560c03576319c973401a
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 15:40:55 -0300

    refactor: componente Service

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,30 +1,7 @@
 import React from 'react';
 import ServiceCard from '../CardService';
-import styled from 'styled-components';
-
-const ServicesContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
-
-const services = [
-  {
-    name: "Troca de óleo",
-    description: "Troca de óleo do motor do veículo.",
-    image: "https://example.com/troca-de-oleo.jpg"
-  },
-  {
-    name: "Alinhamento e balanceamento",
-    description: "Alinhamento e balanceamento das rodas do veículo.",
-    imagem: "https://example.com/alinhamento-e-balanceamento.jpg"
-  },
-  {
-    name: "Revisão preventiva",
-    description: "Revisão completa do veículo para evitar problemas futuros.",
-    imagem: "https://example.com/revisao-preventiva.jpg"
-  }
-];
+import ServicesContainer from './style';
+import services from '../../mocks/services';
 
 function Services() {
   return (
---
+++ src/mocks/services.js
@@ -0,0 +1,19 @@
+const services = [
+  {
+    name: "Troca de óleo",
+    description: "Troca de óleo do motor do veículo.",
+    image: "https://example.com/troca-de-oleo.jpg"
+  },
+  {
+    name: "Alinhamento e balanceamento",
+    description: "Alinhamento e balanceamento das rodas do veículo.",
+    imagem: "https://example.com/alinhamento-e-balanceamento.jpg"
+  },
+  {
+    name: "Revisão preventiva",
+    description: "Revisão completa do veículo para evitar problemas futuros.",
+    imagem: "https://example.com/revisao-preventiva.jpg"
+  }
+];
+
+export default services;
