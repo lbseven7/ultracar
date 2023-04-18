71e70380b936243ae33f7f9758f460f6992c1123
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 12:25:15 -0300

    feat: organizando o componente Cadastro

--- src/components/Cadastro/index.jsx
+++ src/components/Cadastro/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import FormStyle from '../Login/style';
 
 function Cadastro() {
   return (
     <>
-      <h1>Cadastro</h1>
+      <FormStyle>
+      <h2>Cadastrar novo cliente</h2>
+        <label htmlFor="name"> Nome </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Digite o nome do cliente"
+        />
+    
+        <button type="submit">Cadastrar Cliente</button>
+
+        <a
+          href='https://mail.google.com/chat/u/0/#chat/space/AAAACfnje_s'
+          target='_blank'
+          rel="noreferrer">
+          Voltar
+        </a>
+      </FormStyle>
     </>
   );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from '../components/Login';
+import Cadastro from '../components/Cadastro';
 
 
 function Routes() {
   return (
     <BrowserRouter>
+      {/* <Route path="/" exact component={Home} /> */}
       <Route path="/login" component={Login} />
+      <Route path="/cadastro" component={Cadastro} />
     </BrowserRouter>
   )
 }
