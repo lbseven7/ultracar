4474c6c7ea4d641f6d80d24d2d2e59071620f2f9
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 09:41:43 -0300

    feat: adiciona arquivo de rotas

--- src/components/ClientsId.jsx
+++ src/components/ClientsId.jsx
@@ -1,51 +1,65 @@
 import React from 'react';
-import { clientes } from '../mocks/clientes';
+// import clientes from '../mocks/clientes';
 
 
 function IdClients() {
 
-  const [cliente, setCliente] = React.useState(null);
-  const [qrCode, setQrCode] = React.useState("");
-
-  function handleQrCode(qrCode) {
-    setQrCode(qrCode);
-
-    const clienteEncontrado = clientes.find((cliente) => cliente.qrCode === qrCode);
-    if (clienteEncontrado) {
-      setCliente(clienteEncontrado);
+  const clientes = [
+    {
+      "id": 1,
+      "nome": "João da Silva",
+      "telefone": "(11) 9999-9999",
+      "endereco": "Rua A, 123 - São Paulo/SP",
+      "veiculo":
+      {
+        "marca": "Fiat",
+        "modelo": "Uno",
+        "ano": 2020,
+        "placa": "ABC-1234",
+        "qrCode": 569999542
+      }
+    },
+    {
+      "id": 2,
+      "nome": "Maria Santos",
+      "telefone": "(11) 8888-8888",
+      "endereco": "Rua B, 456 - São Paulo/SP",
+      "veiculo":
+      {
+        "marca": "Volkswagen",
+        "modelo": "Gol",
+        "ano": 2018,
+        "placa": "DEF-5678",
+        "qrCode": 987456321
+      }
+    },
+    {
+      "id": 3,
+      "nome": "José Pereira",
+      "telefone": "(11) 7777-7777",
+      "endereco": "Rua C, 789 - São Paulo/SP",
+      "veiculo":
+      {
+        "marca": "Ford",
+        "modelo": "Ka",
+        "ano": 2019,
+        "placa": "GHI-9012",
+        "qrCode": 123456789
+      }
     }
-  }
+   ]
 
   return (
     <div>
-      <h2>Identificação do cliente</h2>
-      <label htmlFor="qrCode">Leitura do QRCode</label>
-      <input
-        id="qrCode"
-        type="text"
-        value={qrCode}
-        onChange={(event) => handleQrCode(event.target.value)}
-      />
-      {
-        cliente && (
-            <form>
-        <label htmlFor="name">Nome</label>
-        <input type="text" id="name" name="name" />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="cpf">CPF</label>
-        <input type="text" id="cpf" name="cpf" />
-        <label htmlFor="address">Endereço</label>
-        <input type="text" id="address" name="address" />
-        <label htmlFor="city">Cidade</label>
-        <input type="text" id="city" name="city" />
-        <label htmlFor="state">Estado</label>
-        <select >
-          <option value="SP">São Paulo</option>
-        </select>
+      <h2>Cliente já cadastrado?</h2>
+   
+      <form>
+        <label htmlFor="name"> Nome </label>
+        <input type="text" id="name" name="name"/>
+        <label htmlFor="qrcode"> QRCode </label>
+        <input type="qrcode" id="qrcode" name="qrcode" />
+        <button type="submit">Enviar</button>
       </form>
-        )
-      }
     </div>
   );
 }
--- src/mocks/clientes.js
+++ src/mocks/clientes.js
@@ -1,6 +1,6 @@
- export const clientes = [
+ const clientes = [
   {
-    "id": "1",
+    "id": 1,
     "nome": "João da Silva",
     "telefone": "(11) 9999-9999",
     "endereco": "Rua A, 123 - São Paulo/SP",
@@ -8,13 +8,13 @@
     {
       "marca": "Fiat",
       "modelo": "Uno",
-      "ano": "2020",
+      "ano": 2020,
       "placa": "ABC-1234",
-      "qrCode": "0123456789"
+      "qrCode": 569999542
     }
   },
   {
-    "id": "2",
+    "id": 2,
     "nome": "Maria Santos",
     "telefone": "(11) 8888-8888",
     "endereco": "Rua B, 456 - São Paulo/SP",
@@ -22,13 +22,13 @@
     {
       "marca": "Volkswagen",
       "modelo": "Gol",
-      "ano": "2018",
+      "ano": 2018,
       "placa": "DEF-5678",
-      "qrCode": "9876543210"
+      "qrCode": 987456321
     }
   },
   {
-    "id": "3",
+    "id": 3,
     "nome": "José Pereira",
     "telefone": "(11) 7777-7777",
     "endereco": "Rua C, 789 - São Paulo/SP",
@@ -36,9 +36,11 @@
     {
       "marca": "Ford",
       "modelo": "Ka",
-      "ano": "2019",
+      "ano": 2019,
       "placa": "GHI-9012",
-      "qrCode": "1234567890"
+      "qrCode": 123456789
     }
   }
-]
+ ]
+
+ export default clientes;
