0e77e387b2e4e920ed01511651bf235077be7c4d
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 15:42:50 -0300

    chore: exclui componente Logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Logo from '../Logo';
+import Logo from '../../assets/images/Logo Nova-04.png';
 import ContainerStyle from './style';
 
 function Header() {
   return (
     <ContainerStyle>
-       <Logo />
+       <img src={Logo} alt="Logo" />
     </ContainerStyle>
   );
 }
