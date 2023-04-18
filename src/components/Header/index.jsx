9ab6974a38f5e77e30a03dddbc18b202f4d675fb
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 17:35:03 -0300

    chore: troca de logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/images/Logo Nova-04.png';
+import Logo from '../../assets/images/cabecalho.png';
 import ContainerStyle from './style';
 
 function Header() {
