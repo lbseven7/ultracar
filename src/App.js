205458c316ecf89ee7d60f8e87912d213f720dce
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 12:23:00 -0300

    feat: cria componente Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+// import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+     <p>Página Principal</p>
+     <p>side-bar</p>
+     <p>cards com os serviços oferecidos</p>
+    </>
+  );
+}
+
+export default Home;
