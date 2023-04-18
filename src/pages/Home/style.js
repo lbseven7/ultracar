0505d6c52c695ed4c4ca5f5b7f1a755be10930e2
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 17:37:46 -0300

    style: estilo do SideBar

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -2,4 +2,15 @@ import styled from 'styled-components';
 
 export const ContainerStyle = styled.div`
   text-align: center;
+
+  > main {
+    display: flex;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+
+    }
+  }
 `;
