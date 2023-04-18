3fe8a34ffe9d230d0492c6b36264481e169ee556
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 17:36:38 -0300

    style: estilo dos links do SideBar

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,24 +1,38 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const SideBarContainer = styled.div`
-  width: 250px;
+export const SideBarContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 15%;
   height: 100vh;
-  background-color: #f0f0f0;
-  padding: 20px;
-`;
-
-export const SidebarLink = styled.a`
-  display: block;
-  font-size: 1.2rem;
-  margin-bottom: 10px;
-  color: #333;
-  text-decoration: none;
-  &:hover {
-    color: #555;
+  border: 1px solid #ccc;
+  
+  > img {
+    width: 90px;
+    margin-top: 20px;
   }
 `;
 
-  export const SidebarHeading = styled.h2`
+export const SidebarHeading = styled.h2`
   font-size: 1.5rem;
   margin-bottom: 20px;
 `;
+
+export const LinkStyle = styled(Link)`
+  text-decoration: none;
+  width: 95%;
+  margin-top: 20px;
+  line-height: 50px;
+  color: #1490c5;
+  border: 1px solid #ccc;
+  transition: all 0.4s ease-in-out;
+
+  &:hover {
+    background-color: #1490c5;
+    color: #fff;
+  }
+`;
+
+
