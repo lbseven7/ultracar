2125e744860f7ab0f7077e252ffe89a5a6f9af56
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-18 21:31:44 -0300

    feat: adiciona serviços ao mock

--- src/mocks/services.js
+++ src/mocks/services.js
@@ -2,17 +2,43 @@ const services = [
   {
     name: "Troca de óleo",
     description: "Troca de óleo do motor do veículo.",
-    image: "https://example.com/troca-de-oleo.jpg"
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
   },
   {
     name: "Alinhamento e balanceamento",
     description: "Alinhamento e balanceamento das rodas do veículo.",
-    imagem: "https://example.com/alinhamento-e-balanceamento.jpg"
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
   },
   {
     name: "Revisão preventiva",
     description: "Revisão completa do veículo para evitar problemas futuros.",
-    imagem: "https://example.com/revisao-preventiva.jpg"
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
+  },
+  {
+    name: "Motor",
+    description: "Revisão completa do motor do veículo.",
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
+  },
+  {
+    name: "Freios",
+    description: "Revisão completa dos freios do veículo.",
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
+  },
+  {
+    name: "Suspensão",
+    description: "Revisão completa da suspensão do veículo.",
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
+  },
+  {
+    name: "Ar condicionado",
+    description: "Revisão completa do ar condicionado do veículo.",
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
+  },
+  {
+    name: "Elétrica",
+    description: "Revisão completa do sistema elétrico do veículo.",
+    image: "https://production.autoforce.com/uploads/service/banner/2661/main_webp_comprar-bmw-oleo-original_c3397a6dab.png.webp"
+
   }
 ];
 
